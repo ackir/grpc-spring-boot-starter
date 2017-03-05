903ee4680d029add21d6ccfb2d176d85ba05d305
Kirill Tolkachev <lavcraft@gmail.com>
2017-03-05 22:40:48 +0300

    Prepare for release via circleci

--- gradle/buildViaCircleCI.sh
+++ gradle/buildViaCircleCI.sh
@@ -7,15 +7,14 @@ GRADLE_VERSION=$(./gradlew -version | grep Gradle | cut -d ' ' -f 2)
 echo "CI_PULL_REQUEST:$CI_PULL_REQUEST"
 echo "CIRCLE_TAG:$CIRCLE_TAG"
 echo "CIRCLE_BRANCH:$CIRCLE_BRANCH"
-echo "TEST:$TEST"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$CIRCLE_BRANCH]"
+if [ "x$CI_PULL_REQUEST" != "x" ]; then
+  echo -e "Build Pull Request #$CI_PULL_REQUEST => Branch [$CIRCLE_BRANCH]"
   ./gradlew build $SWITCHES
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$CIRCLE_TAG" == "" ]; then
+elif [ "x$CI_PULL_REQUEST" == "x" ] && [ "x$CIRCLE_TAG" == "x" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$CIRCLE_BRANCH']'
   ./gradlew -Prelease.travisci=true snapshot $SWITCHES
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$CIRCLE_TAG" != "" ]; then
+elif [ "x$CI_PULL_REQUEST" == "x" ] && [ "x$CIRCLE_TAG" != "x" ]; then
   echo -e 'Build Branch for Release => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']'
   case "$CIRCLE_TAG" in
   *-rc\.*)
@@ -26,7 +25,7 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$CIRCLE_TAG" != "" ]; then
     ;;
   esac
 else
-  echo -e 'WARN: Should not be here => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
+  echo -e 'WARN: Should not be here => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']  Pull Request ['$CI_PULL_REQUEST']'
   ./gradlew build $SWITCHES
 fi
 
